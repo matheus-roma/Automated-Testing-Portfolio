142966446df5c9385b9080fc88f89d498f278e67
matheus-roma <matheus.roma@ufpe.br>
2022-11-26 20:36:53 -0300

    first success

--- testAutomation/index.js
+++ testAutomation/index.js
@@ -1,7 +1,9 @@
-const{Builder, By, Key, util} = require("selenium-webdriver");
+const{Builder, By, Key, until} = require("selenium-webdriver");
 async function testBase(){
-    let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548");
-    await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+    let driver = new Builder().forBrowser("firefox").build();
+    driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548");
+    driver.wait(until.elementLocated(By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div/div/input[1]")), 10000).sendKeys("Teste", Key.RETURN);
+
+
 }
 testBase();

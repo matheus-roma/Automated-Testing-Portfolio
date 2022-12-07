3dd8e44ded0ce4842a1d4fd8f2bf2bbc54abd7d6
matheus-roma <matheus.roma@ufpe.br>
2022-12-07 09:46:34 -0300

    separating constants

---
+++ testAutomation/constants.js
@@ -0,0 +1,36 @@
+const PAGE_URL = "https://forms.liferay.com/web/forms/shared/-/form/122548"
+
+const idBR = "_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_kldx______menu__portugues_2d_brasil__1"
+
+const xPathFeedbackItems1 =  "/html/body/div[1]/div[1]/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div/div/span[2]/div"
+const xPathFeedbackItems2 =  "/html/body/div[1]/div[1]/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/span[2]/div"
+const xPathFeedbackItems3 =  "/html/body/div[1]/div[1]/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[2]/div/div/div/div/div/span[2]/div"
+
+const feedbackEmptyField = "This field is required."
+const languageButtonId = "_com_liferay_dynamic_data_mapping_form_web_portlet_DDMFormPortlet_kldx___menu"
+const formPageTitleClassName = "lfr-ddm-form-page-title"
+const pageHeaderTitleClassName = "lfr-ddm__default-page-header-title"
+const pageHeaderDescriptionClassName = "lfr-ddm__default-page-header-description"
+const formPageDescriptionClassName = "lfr-ddm-form-page-description"
+const textSecondaryClassName = "text-secondary"
+const firstQuestionXPath = "/html/body/div[3]/div/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div/div/label"
+const secondQuestionXPath = "/html/body/div[3]/div/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/label"
+const thirdQuestionXPath = "/html/body/div[2]/div/div/div/div/div/div/form/div[4]/div[2]/div/div/div[1]/div/div/div[2]/div/div/div/div/div/label"
+const submitTextId = "ddm-form-submit"
+
+exports.PAGE_URL = PAGE_URL
+exports.idBR = idBR
+exports.xPathFeedbackItems1 = xPathFeedbackItems1
+exports.xPathFeedbackItems2 = xPathFeedbackItems2
+exports.xPathFeedbackItems3 = xPathFeedbackItems3
+exports.feedbackEmptyField = feedbackEmptyField
+exports.languageButtonId = languageButtonId
+exports.formPageTitleClassName = formPageTitleClassName
+exports.pageHeaderTitleClassName = pageHeaderTitleClassName
+exports.pageHeaderDescriptionClassName = pageHeaderDescriptionClassName
+exports.formPageDescriptionClassName = formPageDescriptionClassName
+exports.textSecondaryClassName = textSecondaryClassName
+exports.firstQuestionXPath = firstQuestionXPath
+exports.secondQuestionXPath = secondQuestionXPath
+exports.thirdQuestionXPath = thirdQuestionXPath
+exports.submitTextId = submitTextId
